8433a9c5a209c16b2b38bc9cee0256e0611f55aa
Pesha375 <evanpeshawar@gmail.com>
2024-11-23 16:37:03 +0300

    Add templates and views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,13 +2,7 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from relationship_app import views # type: ignore
 
-from django.urls import path
-from . import views
-
 urlpatterns = [
-    # Function-based view
-    path('books/', views.list_books, name='list_books'),
-    
-    # Class-based view
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),  # Function-based view
+     path('library/<int:library_id>/', views.library_detail, name='library_detail'), # Class-based view
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Book, Library
+from django.views import View
+from django.views.generic import ListView, DetailView
+from.models import Book, Library, Author
 
-# Function-based view to list all books
 def list_books(request):
-    books = Book.objects.select_related('author').all()  # Fetch all books with related authors
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
 
-# Class-based view to display library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    template_name = 'library_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        library = self.get_object()
+        context['library'] = library
+        return context
