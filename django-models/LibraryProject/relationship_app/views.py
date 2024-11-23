7b499aa8342470bec3a00f075c1e6ae633a8750d
Pesha375 <evanpeshawar@gmail.com>
2024-11-23 17:53:22 +0300

    Add templates and views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,8 @@ from django.views import View
 from django.views.generic import ListView, DetailView
 from.models import Book, Library, Author
 from .models import Library, Book
+from django.views.generic.detail import DetailView
+
 
 def list_books(request):
     books = Book.objects.all()
