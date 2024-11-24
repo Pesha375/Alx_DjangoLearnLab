2a2555b787d90afeee6e0846e69a8da059172764
Pesha375 <evanpeshawar@gmail.com>
2024-11-24 10:07:23 +0300

    Add Authentication implementation

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from relationship_app import views # type: ignore
-
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register
 
 
 urlpatterns = [
   path('books/', list_books, name='list_books'),  # Function-based view
   path('library/<int:library_id>/', LibraryDetailView.as_view(), name='library_detail'),   
-  path('login/', views.login_view, name='login'),
-  path('logout/', views.logout_view, name='logout'),
-  path('register/', views.register_view, name='register'),
-    
+  # Login view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+
+    # Logout view
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # Registration view
+    path('register/', register, name='register'),
     
 ]
