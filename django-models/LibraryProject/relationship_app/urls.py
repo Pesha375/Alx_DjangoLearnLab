ea75a8f3e8952213d2fe10c53e509352040affd7
Pesha375 <evanpeshawar@gmail.com>
2024-11-24 10:04:00 +0300

    Add Authentication implementation

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
   path('library/<int:library_id>/', LibraryDetailView.as_view(), name='library_detail'),   
   path('login/', views.login_view, name='login'),
   path('logout/', views.logout_view, name='logout'),
-  path('register/', views.register_view, name='register'),   
-    
+  path('register/', views.register_view, name='register'),
     
     
 ]
