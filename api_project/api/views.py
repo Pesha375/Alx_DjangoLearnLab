f6f590f36dddbc5aea32e1480c84faa95cfdba1c
Pesha375 <evanpeshawar@gmail.com>
2024-12-06 17:37:06 +0300

    Implementation of DRF Serializer

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render # type: ignore
 # Create your views here.
 # views.py
 from rest_framework.generics import ListAPIView # type: ignore
-from .models import Book
-from .serializers import BookSerializer
+from.models import Book
+from.serializers import BookSerializer
 
 class BookList(ListAPIView):
-    queryset = Book.objects.all()  # Retrieve all books
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
