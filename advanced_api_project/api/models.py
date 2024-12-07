aeda72d223548e928f575e26e059bbf2749974be
Pesha375 <evanpeshawar@gmail.com>
2024-12-07 20:04:21 +0300

    Initial setup for advanced API project with custom serializers

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,27 +1,9 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
-# Author Model
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.name
-
-# Book Model
 class Book(models.Model):
     title = models.CharField(max_length=255)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-# Author model represents a book's author with a name field.
-# Books are related to authors through a one-to-many relationship.
-
-
-
-
--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers # type: ignore
 from .models import Author, Book
 from datetime import datetime
 
