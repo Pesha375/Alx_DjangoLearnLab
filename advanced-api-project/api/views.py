c27f80e12a9bcbb112f11a5c5a3a3ea93d6d391b
Pesha375 <evanpeshawar@gmail.com>
2024-12-09 20:53:13 +0300

    Updtated views.py file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 from .permissions import IsOwnerOrReadOnly
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 # List all books
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
