f12dfcbfde6de875ac9910b358584bd0010c2bb6
Pesha375 <evanpeshawar@gmail.com>
2024-12-12 16:07:52 +0300

    Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,14 @@
+# blog/urls.py
 from django.urls import path
-from . import views  # Correct the import statement
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('posts/', views.post_list, name='posts'),
+    path('', views.PostListView.as_view(), name='posts'),
+    path('posts/', views.PostListView.as_view(), name='posts'),
+    path('posts/new/', views.PostCreateView.as_view(), name='post_new'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register, name='register'),
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render, redirect
+# blog/views.py
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib import messages
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, PostForm
+from .models import Post
 from django.contrib.auth.models import User
+
+# Authentication Views
 
 def register(request):
     if request.method == 'POST':
@@ -48,9 +50,51 @@ def profile(request):
         return redirect('profile')
     return render(request, 'blog/profile.html', {'user': request.user})
 
+# Home View
+
 def home(request):
     return render(request, 'blog/home.html')
 
+# Blog Post Views
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# Function-based view for post_list (optional, for consistency with other function-based views)
 def post_list(request):
-    posts = Post.objects.all() # type: ignore
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
